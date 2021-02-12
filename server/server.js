3c30375a9a1f2ba6a062648a040792e8c4568cb3
noordseoostende <voropayev1@mail.ru>
2021-02-12 11:24:34 +0100

    de mongoserver was gemaakt

--- server/server.js
+++ server/server.js
@@ -2,16 +2,46 @@ const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require('body-parser');
+const { buildSchema } = require('graphql');
+const expressPlayground = require('graphql-playground-middleware-express').default
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
+app.get('/playground',expressPlayground({endpoint: '/graphql'}))
 app.use(
   '/graphql',
   graphqlHTTP({
+    schema:buildSchema(`
+    type RootQuery {
+      hello: String!
+    }
+    type RootMutation{
+      somemutation:String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+    `),
+    rootValue:{
+      hello:()=>{
+        return 'Hello Jack.'
+      }
+    },
+    graphiql: true
   
 }))
-
+//mongodb+srv://graphqluser:<password>@cluster0.2y4bh.mongodb.net/<dbname>?retryWrites=true&w=majority
 const PORT = process.env.PORT || 5000;
+mongoose.connect(`mongodb+srv://graphqluser:testing123@cluster0.2y4bh.mongodb.net/<dbname>?retryWrites=true&w=majority`,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+}).then(()=>{
 
-app.listen(PORT,()=>{
-  console.log(`Het werkt uitstekend ${PORT}`)
+  app.listen(PORT,()=>{
+    console.log(`Het werkt uitstekend ${PORT}`)
+  });
+}).catch( err => {
+  console.log(err)
 });
